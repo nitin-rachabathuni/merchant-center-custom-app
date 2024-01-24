cb7f64bd320f323dfb1e3bbe43b930b8dec26d8b
Nitin Rachabathuni <>
2024-01-24 23:20:26 +0530

    init

---
+++ custom-application-config.mjs
@@ -0,0 +1,39 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Starter 904c81',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-au',
+  env: {
+    development: {
+      initialProjectKey: 'kitchenwarehouse-dev',
+    },
+    production: {
+      applicationId: 'TODO',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
